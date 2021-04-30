738cea377c652c0b7ca0b5d93c1dd36dab474240
SeolHuiGwan9478 <shg5977@naver.com>
2021-05-01 02:32:58 +0900

    variable js

---
+++ variable.js
@@ -0,0 +1,22 @@
+//console.log 는 웹 브라우저의 콘솔 창에 출력을 하는 것
+console.log('Hello JavaScript!!');
+//변수, 상수
+// 변수는 let 키워드를 사용해서 선언. 같은 이름의 변수를 선언할 수 없다. 지역변수는 가능
+// 상수는 const 키워드를 사용. 
+// var은 현재 사용하지 않음. var은 같은 이름 변수 여러가지 가능 
+let value = 1;
+const a = 2;
+// 데이터 타입
+//1. numeric
+let value = 1;
+//2. 문자열
+let text = 'hello';
+let name = "자바스크립트";
+//3.참/거짓
+let good = true;
+let loading = false;
+//4. null & undefined
+//null과 undefined의 차이점은 null은 고의적으로 값이 없다고 지정하는 것이고, undefined는 우리가 설정을 아직 하지 않았기 때문이다.
+const friend = null;
+let criminal;
+console.log(criminal);
