8d65d107444fbad7bed8176aa118238c35d00433
SeolHuiGwan9478 <shg5977@naver.com>
2022-03-07 18:27:31 +0900

    test

--- browser_js/index.js
+++ browser_js/index.js
@@ -7,16 +7,31 @@
 //title.style.color = 'red';
 
 //DOM 트리 여행하기
-const myTag = document.querySelector()
+//const myTag = document.querySelector()
 
 //부모 요소 노드
-console.log(myTag.parentElement);
+//console.log(myTag.parentElement);
 
 //자식 요소 노드
-console.log(myTag.children[1]);
-console.log(myTag.firstElementChild);
-console.log(myTag.lastElementChild);
+// console.log(myTag.children[1]);
+// console.log(myTag.firstElementChild);
+// console.log(myTag.lastElementChild);
 
 //형제 요소 노드
-console.log(myTag.previousElementSibling);
-console.log(myTag.nextElementSibling);
+// console.log(myTag.previousElementSibling);
+// console.log(myTag.nextElementSibling);
+
+//요소 노드 주요 프로퍼티
+const myTag = document.querySelector("#list-1");
+
+//innerHTML
+// console.log(myTag.innerHTML);
+// myTag.innerHTML += '<li>Exotic</li>';
+// myTag.innerHTML = '<li>Exotic</li>';
+
+//outerHTML
+console.log(myTag.outerHTML);
+
+//textContent
+console.log(myTag.textContent);
+myTag.textContent = '<li>test</li>';
