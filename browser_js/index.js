dff5564c882a4a496d04c0a6aa31b0ecf6f0b464
SeolHuiGwan9478 <shg5977@naver.com>
2022-03-07 16:16:46 +0900

    add interactive_js, browser_js folder

---
+++ browser_js/index.js
@@ -0,0 +1,22 @@
+//DOM(Document object model)
+//console.log(document);
+//console.log(typeof document);
+//console.dir(document);
+//
+//const title = document.querySelector('#title');
+//title.style.color = 'red';
+
+//DOM 트리 여행하기
+const myTag = document.querySelector()
+
+//부모 요소 노드
+console.log(myTag.parentElement);
+
+//자식 요소 노드
+console.log(myTag.children[1]);
+console.log(myTag.firstElementChild);
+console.log(myTag.lastElementChild);
+
+//형제 요소 노드
+console.log(myTag.previousElementSibling);
+console.log(myTag.nextElementSibling);
