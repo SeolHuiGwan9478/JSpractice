d8adcd68ba720c588c75236d620aafa6b9eeae8e
SeolHuiGwan9478 <shg5977@naver.com>
2022-03-13 17:27:04 +0900

    add delete,move node

--- browser_js/index.js
+++ browser_js/index.js
@@ -36,36 +36,46 @@
 // console.log(myTag.textContent);
 // myTag.textContent = '<li>test</li>';
 
+// const today = document.querySelector('#today');
+// today.innerHTML = '<li>처음</li>' + today.innerHTML;
+// today.innerHTML = today.innerHTML + '<li>마지막</li>';
+
+// today.outerHTML = '<p>이전</p>' + today.outerHTML;
+// console.log(today.outerHTML);
+
+// const newToday = document.querySelector('#today');
+// newToday.outerHTML = newToday.outerHTML + '<p>다음</p>';
+
+// //요소 노드 추가하기
+// const tomorrow = document.querySelector('#tomorrow');
+
+// //1. 요소 노드 만들기: document.createElement('태그이름');
+// const first = document.createElement('li');
+
+// //2. 요소 노드 꾸미기: textContent, innerHTML, ...을 통해서
+// first.textContent = '처음';
+
+// //3. 요소 노드 추가하기 : NODE.prepend, append, after, before
+// tomorrow.prepend(first);
+
+// const last = document.createElement('li');
+// last.textContent = '마지막';
+// tomorrow.append(last);
+
+// const prev = document.createElement('p');
+// prev.textContent = '이전';
+// tomorrow.before(prev);
+
+// const next = document.createElement('p');
+// next.textContent = '다음';
+// tomorrow.after(next);
+
+//노드 삭제하기 : Node.remove()
 const today = document.querySelector('#today');
-today.innerHTML = '<li>처음</li>' + today.innerHTML;
-today.innerHTML = today.innerHTML + '<li>마지막</li>';
-
-today.outerHTML = '<p>이전</p>' + today.outerHTML;
-console.log(today.outerHTML);
-
-const newToday = document.querySelector('#today');
-newToday.outerHTML = newToday.outerHTML + '<p>다음</p>';
-
-//요소 노드 추가하기
 const tomorrow = document.querySelector('#tomorrow');
 
-//1. 요소 노드 만들기: document.createElement('태그이름');
-const first = document.createElement('li');
+//tomorrow.remove();
 
-//2. 요소 노드 꾸미기: textContent, innerHTML, ...을 통해서
-first.textContent = '처음';
-
-//3. 요소 노드 추가하기 : NODE.prepend, append, after, before
-tomorrow.prepend(first);
-
-const last = document.createElement('li');
-last.textContent = '마지막';
-tomorrow.append(last);
-
-const prev = document.createElement('p');
-prev.textContent = '이전';
-tomorrow.before(prev);
-
-const next = document.createElement('p');
-next.textContent = '다음';
-tomorrow.after(next);
+//노드 이동하기 : prepend, append, before, after
+tomorrow.children[1].after(today.children[1]);
+//tomorrow.children[2].before(today.children[1]);
