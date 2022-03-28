66ed0a50498e138d21c549d04be53990ffcffce4
SeolHuiGwan9478 <shg5977@naver.com>
2022-03-28 12:57:31 +0900

    resolve home_hw

--- home_hw/index.js
+++ home_hw/index.js
@@ -1,15 +1,23 @@
 // showTitle 함수를 완성해 주세요.
 function showTitle(e) {
 	// 여기에 코드를 작성해 주세요.
-    console.log(e.target);
+    if(e.target.getAttribute('data-title')){
+		const spanTag = document.createElement('span');
+		const content = e.target.dataset.title;
+		spanTag.classList.add('title');
+		spanTag.textContent = content;
+		e.target.append(spanTag);
+	}
 }
 
 // removeTitle 함수를 완성해 주세요.
-//function removeTitle(e) {
+function removeTitle(e) {
 	// 여기에 코드를 작성해 주세요.
-
-//}
+	if(e.target.getAttribute('data-title')){
+		e.target.lastElementChild.remove();
+	}
+}
 const map = document.querySelector(".map");
 // '대상'과 '타입'을 수정해 주세요.
 map.addEventListener("mouseover", showTitle);
-//대상.addEventListener('타입', removeTitle);
+map.addEventListener('mouseout', removeTitle);
